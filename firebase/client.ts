afa372f1fbf7a7b62820707fd564f8f62d8117f9
Visha193 <Vishal12kl@gmail.com>
2025-06-12 13:08:27 +0530

    changed some code in client.js

--- firebase/client.ts
+++ firebase/client.ts
@@ -13,7 +13,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
