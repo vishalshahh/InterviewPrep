a9e974c7785b7d2a0cdb40a0ed76e4d1326e13a2
Visha193 <Vishal12kl@gmail.com>
2025-06-12 13:32:56 +0530

    change in client.js

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyC-AyTEsTjBC_r6i86AC4HzEPgxr6a7CYY",
+  authDomain: "interviewprep-25202.firebaseapp.com",
+  projectId: "interviewprep-25202",
+  storageBucket: "interviewprep-25202.firebasestorage.app",
+  messagingSenderId: "510685160053",
+  appId: "1:510685160053:web:661e4906faade728c44e3c",
+  measurementId: "G-VN6BRY3M3Y",
 };
 
 // Initialize Firebase
--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint:{
+  eslint: {
     ignoreDuringBuilds: true, // Ignore ESLint errors during build
   },
   typescript: {
     ignoreBuildErrors: true, // Ignore TypeScript errors during build
-  }
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
